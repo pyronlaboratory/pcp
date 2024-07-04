a863370e5dfcad3d2d74dc885bae012a72d29dc3
Ronnie <ronnie94official@gmail.com>
2024-07-04 14:26:24 +0100

    Update parallelwalk.py+++

--- pcplib/parallelwalk.py
+++ pcplib/parallelwalk.py
@@ -1,6 +1,7 @@
 # Copyright Genome Research Ltd 2014
 # Author gmpc@sanger.ac.uk
 # This program is released under the GNU Public License V2 or later (GPLV2+)
+
 from mpi4py import MPI
 import os
 import random
